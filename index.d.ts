aa9cf51d6daef00a1b532e405c5706d461ce8e19
Pasqualino Titto Assini <tittoassini@gmail.com>
2018-03-06 17:06:51 +0100

    release

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
-import { Flat, flat, zmType, zmId, zmFold, unflat, flatDecoder, Decoder } from './api';
-import { typedBLOB, Channel, Client, channel, CallChannel, promiseTimeout } from './top';
+import { Flat, flat, zmType, zmId, zmFold, unflat, flatDecoder, Decoder } from './lib/api';
+import { typedBLOB, Channel, Client, channel, CallChannel, promiseTimeout } from './lib/top';
 export { Flat, flat, zmType, zmId, zmFold, unflat, flatDecoder, Decoder, typedBLOB, Channel, Client, channel, CallChannel, promiseTimeout };
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import {Flat,flat,zmType,zmId,zmFold,unflat,flatDecoder,Decoder} from './api'
+import {Flat,flat,zmType,zmId,zmFold,unflat,flatDecoder,Decoder} from './lib/api'
 
-import {typedBLOB,Channel,Client,channel,CallChannel,promiseTimeout} from './top'
+import {typedBLOB,Channel,Client,channel,CallChannel,promiseTimeout} from './lib/top'
 
 export {Flat,flat,zmType,zmId,zmFold,unflat,flatDecoder,Decoder,typedBLOB,Channel,Client,channel,CallChannel,promiseTimeout};
