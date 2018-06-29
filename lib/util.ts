690ca5b944f636669b3d02b3d02efc47918f9221
Pasqualino Titto Assini <tittoassini@gmail.com>
2018-06-29 12:10:05 +0200

    release

--- lib/util.ts
+++ lib/util.ts
@@ -7,11 +7,11 @@ interface Pretty<A> {
     pretty<A>(v: A): string
 }
 
-export function prettyShake48<I extends Flat>(s: SHAKE128_48<I>): string {
-    function asHex(v: Word8) { const r = v.value.toString(16); return r.length == 1 ? '0' + r : r; }
-    return asHex(s._0) + asHex(s._1) + asHex(s._2) + asHex(s._3) + asHex(s._4) + asHex(s._5);
-}
+export function parseAbsRef(s: string): AbsRef {
+    if (s.substring(0, 1) !== "K") throw "unexpected input: " + s;
 
+    return new AbsRef(parseShake48(s.substring(1)));
+}
 
 export function parseShake48(s: string) {
     function hex(offset: number) { return new Word8(parseInt(s.substring(offset, offset + 2), 16)); }
@@ -21,4 +21,9 @@ export function parseShake48(s: string) {
 
 export function prettyAbsRef(r: AbsRef): string {
     return "K" + prettyShake48(r._0);
+}
+
+export function prettyShake48<I extends Flat>(s: SHAKE128_48<I>): string {
+    function asHex(v: Word8) { const r = v.value.toString(16); return r.length == 1 ? '0' + r : r; }
+    return asHex(s._0) + asHex(s._1) + asHex(s._2) + asHex(s._3) + asHex(s._4) + asHex(s._5);
 }
