57a67757d628d8181c4e4c9a2cdfe8d87676f65b
Pasqualino Titto Assini <tittoassini@gmail.com>
2018-07-07 11:31:01 +0200

    release

--- index.ts
+++ index.ts
@@ -4,4 +4,7 @@ import { typedBLOB, Channel, Client, channel, CallChannel, promiseTimeout } from
 
 import { parseShake48, prettyShake48, prettyAbsRef, parseAbsRef } from './lib/util'
 
+import { pretty} from './lib/pretty'
+
 export { Flat, flat, zmType, zmId, zmFold, unflat, flatDecoder, Decoder, typedBLOB, Channel, Client, channel, CallChannel, promiseTimeout, parseShake48, prettyShake48, prettyAbsRef, parseAbsRef };
+
