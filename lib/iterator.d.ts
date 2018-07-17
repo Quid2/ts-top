9e22cf72ef3a66a4cd911be0f3f824a8d25a4485
Pasqualino Titto Assini <tittoassini@gmail.com>
2018-07-17 11:27:19 +0200

    removed custom classes

---
+++ lib/iterator.d.ts
@@ -0,0 +1,7 @@
+import { ZM } from '../lib/api';
+declare module '../ADT/List/Kb8cd13187198' {
+    interface Nil<A extends ZM> extends Iterable<A> {
+    }
+    interface Cons<A extends ZM> extends Iterable<A> {
+    }
+}
