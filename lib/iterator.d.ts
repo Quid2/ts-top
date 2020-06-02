7593bb74f281c29bdd023eb70ce6fab54abd701d
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-06-02 17:12:02 +0200

    removed files

--- lib/iterator.d.ts
+++
@@ -1,7 +0,0 @@
-import { ZM } from '../lib/api';
-declare module '../ADT/List/Kb8cd13187198' {
-    interface Nil<A extends ZM> extends Iterable<A> {
-    }
-    interface Cons<A extends ZM> extends Iterable<A> {
-    }
-}
--- lib/util.d.ts
+++
@@ -1,4 +0,0 @@
-import { SHAKE128_48 } from '../ADT/SHAKE128_48/K9f214799149b';
-import { AbsRef } from '../ADT/AbsRef/K4bbd38587b9e';
-export declare function parseAbsRef(s: string): AbsRef;
-export declare function parseShake48(s: string): SHAKE128_48<any>;
